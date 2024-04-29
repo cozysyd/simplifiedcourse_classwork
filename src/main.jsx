e8fbbadb304639b1caeabbfd28d95acc4cc18b7b
Sydney Spaulding <cozysydmedia@gmail.com>
2024-04-29 11:05:29 -0700

    added string

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-<React>
-  <App />
-</React>;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React>
+    <App />
+  </React>
+);
