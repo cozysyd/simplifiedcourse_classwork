7b6f29980f1392c0c4696e76e04ba88228a84bcd
Sydney Spaulding <cozysydmedia@gmail.com>
2024-04-29 11:17:34 -0700

    reverted back to blank slate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  function counter() {
-    setCount(count + 1);
-  }
-
-  return <h1 onClick={counter}>Counter: {count}</h1>;
+  return <h1>Hello, World!</h1>;
 }
 
 export default App;
